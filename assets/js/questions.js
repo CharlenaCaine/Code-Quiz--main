e5ff77705de670bb6483d40c1e2eda1380b35979
CharlenaCaine <119806338+CharlenaCaine@users.noreply.github.com>
2023-03-30 23:57:24 +0100

    added questions and linked js files

---
+++ assets/js/questions.js
@@ -0,0 +1,62 @@
+const questions = [
+    {
+        question: "What does HTML stand for?",
+        choice: ["Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Markup Language", "Hyper Tool Markup Language"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        choice: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choice: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes"
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choice: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+    {
+        question: "Which of the below CSS properties is used to change the background color of CSS ?",
+        choice: ["bg color", "color-background", "background-color", "color"],
+        answer: "background-color"
+    },
+    {
+        question: "Which of the below CSS class is used to change the text color of CSS ?",
+        choice: ["background-color", "color", "color-background", "None of the above"],
+        answer: "color"
+    },
+    {
+        question: "Which below property of CSS is used to set the indentation of the first line in a block of text ?",
+        choice: ["text-indent property", "text-underlne-property", "text-decoration none", "text-overflow property"],
+        answer: "text-indent property"
+    },
+    {
+        question: "When interpreter encounters an empty statements, what it will do:",
+        choice: ["Shows a warning", "Prompts to complete the statement", "Throws an error", "Ignores the statements"],
+        answer: "<a>"
+    },
+    {
+        question: "In JavaScript, what is a block of statement?",
+        choice: ["Conditional block", "block that combines a number of statements into a single compound statement", "both conditional block and a single statement", "block that contains a single statement"],
+        answer: "block that combines a number of statements into a single compound statement"
+    },
+    {
+        question: "How do we set the default width of the font in CSS?",
+        choice: ["font-stretch", "font-weight", "text-transform", "font-variant"],
+        answer: "font-stretch"
+    },
+    {
+        question: "Which of the below CSS properties represent the order of flex items in the grid container ?",
+        choice: ["order", "float", "overflow", "All of the above"],
+        answer: "order"
+    },
+    {
+        question: "Which of the below is the abbreviation of CSS?",
+        choice: ["Cascade sheets style", "Color and style sheets", "Cascading style sheets", "Coded style sheet"],
+        answer: "Cascading style sheets"
+    },
+]
